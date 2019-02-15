de5c20e77c4fdc8bd22bfa95aded1f24ab6b660b
Adam Urbina <adamurbina@gmail.com>
2019-02-15 14:07:10 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   const main = document.getElementById('main');
-   
+
   main.addEventListener('click', function(event) {
     alert('I was clicked!');
   });
